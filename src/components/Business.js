1a398fd524ef052d3b81a1533ef9a2b4ac80e856
Norman Cade <norman_cade32@yahoo.com>
2023-05-24 14:42:13 -0400

    CardDeal section is complete

--- src/components/Business.js
+++ src/components/Business.js
@@ -31,7 +31,7 @@ const Business = () => {
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>
-          Yo do the business,
+          You do the business,
           <br className="sm:block: hidden" /> we'll handle the money.
         </h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
